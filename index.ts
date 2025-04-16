95ec8b92ebd8f19bb9ea3a75983e630f8514e54e
blesswrld <roninfortnite71@gmail.com>
2025-04-16 05:11:17 +0300

    rarely types symbol and bigint

--- index.ts
+++ index.ts
@@ -1,32 +1,12 @@
-const testOne: null = null; // Фиксируем примитивный тип данных null и присваем ему ТОЛЬКО null
-const testTwo: any = null; // null - частный случай any, поэтому может присваиваться к any
-const testThree: string = null;
-const testFour: number = null;
+let id: symbol = Symbol("id"); // Фиксируем тип данных symbol для глобальной переменной id
 
-const testFive: undefined = undefined; // // Фиксируем примитивный тип данных undefined и присваем ему undefined
-const testSix: any = undefined; // undefined может присваиваться типу any
-const testSeven: string = undefined; // типу string нельзя присвоить значение undefined
+const data = {
+    [id]: 1, // Присваиваем свойству id значение 1
+};
 
-let smth; // any (любое значение)
+console.log(data[id]); // Выводим в консоль значение переменной data
 
-function getRndData() {
-    if (Math.random() < 0.5) {
-        return null; // Возвращаем null если условие true
-    } else {
-        return "   Some data   "; // Иначе возвращаем строку ---   Some data   ---
-    }
-}
+const numOne: bigint = 1n; // Фиксируем тип данных bigint и присваиваем ей значение в 1n
+const numTwo: bigint = 2n;
 
-const data = getRndData(); // Результат функции помещаем в переменную data
-const trimmedData = data ? data.trim() : null; // Делаем проверку data ? если такая переменная существует, используем метод .trim() : или возвращаем null
-
-// --- Тип Null является подтипом всех типов, за исключением типа Undefined, поэтому его единственное значение null совместимо со всеми остальными типами данных.
-
-// class TypeSystem {
-//   static any: any = null; // Ok
-//   static number: number = null; // Ok
-//   static string: string = null; // Ok
-//   static boolean: boolean = null; // Ok
-//   static undefined: undefined = null; // Ok
-//   static null: null = null; // Ok
-// } ---
+console.log(numOne + numTwo); // Выводим в консоль результат сложения двух переменных
