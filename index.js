fb97cbc07edfd80ba04dc4a11f7177caf2e61201
blesswrld <roninfortnite71@gmail.com>
2025-05-01 18:32:54 +0300

    media, format types task, work with enums, interface and so on

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
-// let msg: "Hello" = "Hello";
-// msg = "Hello World";
-
-var port3000 = 3000;
-var port3001 = 3001;
-
-function startServer(protocol, port) {
-    if (port === port3000 || port === port3001) {
-        console.log(
-            "Server started on ".concat(protocol, "://server:").concat(port)
-        );
+// Перечисление с названием TypesOfMedia, которое включает строчные типы video, audio
+var TypesOfMedia;
+(function (TypesOfMedia) {
+    TypesOfMedia["Video"] = "video";
+    TypesOfMedia["Audio"] = "audio";
+})(TypesOfMedia || (TypesOfMedia = {}));
+// Перечисление с названием FormatsOfMedia, которое включает строчные видео-форматы: .mp4, .mov, .mkv, .flv, .webM
+var FormatsOfMedia;
+(function (FormatsOfMedia) {
+    FormatsOfMedia["Mp4"] = ".mp4";
+    FormatsOfMedia["Mov"] = ".mov";
+    FormatsOfMedia["Mkv"] = ".mkv";
+    FormatsOfMedia["Flv"] = ".flv";
+    FormatsOfMedia["WebM"] = ".webM";
+})(FormatsOfMedia || (FormatsOfMedia = {}));
+function playMedia(_a) {
+    var _b =
+            _a === void 0
+                ? {
+                      name: "example",
+                      type: TypesOfMedia.Video,
+                      format: FormatsOfMedia.Mp4,
+                  }
+                : _a,
+        name = _b.name,
+        type = _b.type,
+        format = _b.format,
+        subtitles = _b.subtitles,
+        marks = _b.marks;
+    var marksLog;
+    if (Array.isArray(marks)) {
+        marksLog = marks.join(" ");
+    } else if (typeof marks === "string") {
+        marksLog = marks;
     } else {
-        console.error("Invalid port");
+        marksLog = "Unsupported type of marks";
     }
-    return "Server started";
-}
-
-startServer("https", 3001);
-
-function createAnimation(id, animName, timingFunc, duration, iterCount) {
-    if (timingFunc === void 0) {
-        timingFunc = "ease";
-    }
-    // const elem = document.getElementById(`#${id}`) as HTMLElement;
-    // if (elem) {
+    // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
+    // Если это строка, то просто поместить её в marksLog
+    // Если что-то другое - то marksLog = "Unsupported type of marks"
+    // Не допускайте any!
     console.log(
-        ""
-            .concat(animName, " ")
-            .concat(timingFunc, " ")
-            .concat(duration, " ")
-            .concat(iterCount)
+        "Media "
+            .concat(name)
+            .concat(format, " is ")
+            .concat(type, "\n    Marks: ")
+            .concat(marksLog, "\n    Subtitles: ")
+            .concat(
+                subtitles !== null && subtitles !== void 0 ? subtitles : "none"
+            )
     );
-    // // elem.style.animation = `${animName} ${timingFunc} ${duration} ${iterCount}`;
-    // }
+    // помните что это за оператор ??
+    return "Media started";
 }
-
-createAnimation("id", "fade", "ease", 3, "infinite");
+playMedia({
+    name: "WoW",
+    format: FormatsOfMedia.Mov,
+    type: TypesOfMedia.Video,
+    subtitles: "Hello World!",
+    marks: ["4:30", "5:40"],
+});
--- index.ts
+++ index.ts
@@ -1,23 +1,74 @@
-// Переменная со значениями стоимости воды и электр
-const dataFromControl = {
-    water: 200,
-    el: 350,
-};
+// Перечисление с названием TypesOfMedia, которое включает строчные типы video, audio
 
-// Функция для обработки значений из переменной
-// Запрашиваем типы из dataFromControl
-function checkReadings(data: typeof dataFromControl): boolean {
-    const dataFromUser = {
-        water: 200,
-        el: 350,
-    };
-
-    // Условие для проверки электр и воды
-    if (data.el === dataFromUser.el && data.water === dataFromUser.water) {
-        return true;
-    } else return false;
+enum TypesOfMedia {
+    Video = "video",
+    Audio = "audio",
 }
 
-// Переменная со значением числа ПИ
-const PI = 3.14;
-let PIClone: typeof PI; // Запрашиваем типы из PI
+// Перечисление с названием FormatsOfMedia, которое включает строчные видео-форматы: .mp4, .mov, .mkv, .flv, .webM
+
+enum FormatsOfMedia {
+    Mp4 = ".mp4",
+    Mov = ".mov",
+    Mkv = ".mkv",
+    Flv = ".flv",
+    WebM = ".webM",
+}
+
+// Описание интерфейса, в котором:
+// name - строка
+// type - один из перечисления выше
+// format = один из перечисления выше
+// subtitles - необязательное поле типа строка
+// marks - необязательное поле неизвестного типа
+
+interface MediaProps {
+    name: string;
+    type: TypesOfMedia;
+    format: FormatsOfMedia;
+    subtitles?: string;
+    marks?: unknown;
+}
+
+function playMedia(
+    { name, type, format, subtitles, marks }: MediaProps = {
+        name: "example",
+        type: TypesOfMedia.Video,
+        format: FormatsOfMedia.Mp4,
+    }
+): string {
+    let marksLog;
+
+    if (Array.isArray(marks)) {
+        marksLog = marks.join(" ");
+    } else if (typeof marks === "string") {
+        marksLog = marks;
+    } else {
+        marksLog = "Unsupported type of marks";
+    }
+    // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
+    // Если это строка, то просто поместить её в marksLog
+    // Если что-то другое - то marksLog = "Unsupported type of marks"
+    // Не допускайте any!
+
+    console.log(`Media ${name}${format} is ${type}
+    Marks: ${marksLog}
+    Subtitles: ${subtitles ?? "none"}`);
+    // помните что это за оператор ??
+
+    return "Media started";
+}
+
+playMedia({
+    name: "WoW",
+    format: FormatsOfMedia.Mov,
+    type: TypesOfMedia.Video,
+    subtitles: "Hello World!",
+    marks: ["4:30", "5:40"],
+});
+
+// --- ЗАДАЧА ВЫПОЛНЕНА --- \\
+
+// tsc index.ts (команда в терминале для запуска компилятора ts кода)
+// tsc -help (команда в терминале для помощи с настройками)
+// ts-node index.ts (команда для запуска ноды + ts файл -- удобно для быстрого вывода в консоль)
